85cc2a1b3d8557ea7163e2fa2c42612ccd85ab5c
ch-lorenz <ch.lorenz72@googlemail.com>
2024-03-16 14:41:04 +0100

    New classes TestClass1 and TestClass2

---
+++ test/src/TestClass1.java
@@ -0,0 +1,33 @@
+/**
+ * @author
+ */
+
+/**
+ * TODO
+ *
+ * @author X11LOR
+ * @version $Revision$
+ *
+ */
+public class TestClass1 {
+
+	/**
+	 * 
+	 */
+	public TestClass1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*
+ * $Log$
+ */
