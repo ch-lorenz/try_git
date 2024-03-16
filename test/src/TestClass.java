fb6980974eaf8662afaff1baf83f48d17aa68d54
ch-lorenz <ch.lorenz72@googlemail.com>
2024-03-16 14:11:39 +0100

    SysOut BRA-2024-03-16-01 V2

--- test/src/TestClass.java
+++ test/src/TestClass.java
@@ -6,6 +6,7 @@ public class TestClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world BRA-2024-03-16-01!");
+		System.out.println("V2");
 		System.out.println("------------");
  }
 
